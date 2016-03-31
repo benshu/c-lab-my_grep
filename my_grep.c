a885d91b6eb34b4cce6f31180d51bc6e89359b1d
Hagay Ben-Shushan <hagaybens@gmail.com>
2016-03-31 20:43:57 +0300

    Passing test5 without regular expressions. basic switches support (-n , -i)

--- my_grep.c
+++ my_grep.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
-
+#include <string.h>
+#include <stdbool.h>
+// TDD -n line number -i case insensitive -E
 int main(int argc, char *argv[])
 {
 	FILE *input_file;
 	char *str_to_find=NULL, *line=NULL;
 	size_t line_cap =0;
-	int line_length=0;
-	str_to_find = argv[1];
-	if (argc==3) {
-		input_file = fopen(argv[2],"r");
+	int line_length=0, current_line_num=0,i=0;
+	bool print_line_num=false, case_insensitive=false;
+	i=1;
+	while (*(argv[i])=='-') {
+		if (*(argv[i]+1)=='n') 
+			print_line_num = true;
+		else if (*(argv[i]+1)=='i') 
+			case_insensitive = true;
+		i++;
+		
+	}
+	str_to_find = argv[i];
+	if (i<(argc-1)) {
+		input_file = fopen(argv[++i],"r");
 	} else
 		input_file =stdin;
 	while((line_length = getline(&line, &line_cap, input_file)) > 0){
-		if(strstr(line,str_to_find))
+		current_line_num++;
+		if((case_insensitive && strcasestr(line,str_to_find)) || strstr(line,str_to_find)){
+			if (print_line_num) {
+				printf("%d:", current_line_num);
+			}
 			printf("%s",line);
+		}
 	}
 	free(line);
 	fclose(input_file);
--- run_test.sh
+++ run_test.sh
@@ -5,4 +5,5 @@ make
 ./my_grep bla ./grep_tests/bla | diff ./grep_tests/bla -
 ./my_grep bla ./grep_tests/3lines | diff ./grep_tests/out3 -
 cat ./grep_tests/3lines | ./my_grep bla | diff ./grep_tests/out3 -
-./my_grep -n -i -E "o\.pdf" ./grep_tests/2013.html | diff ./grep_tests/out5 -
+./my_grep -n pdf ./grep_tests/2013.html | diff ./grep_tests/myout5 -
+#./my_grep -n -i -E "o\.pdf" ./grep_tests/2013.html | diff ./grep_tests/out5 -
