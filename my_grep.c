c401e527df8462358f43db57ed1fef0bbad67062
Hagay Ben-Shushan <hagaybens@gmail.com>
2016-04-18 11:52:38 +0300

    Header file added

--- my_grep.c
+++ my_grep.c
@@ -2,33 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include "my_grep.h"
+
 //
 // TODO - fix -b for stdin
 //
-typedef struct params_struct{
-	bool print_line_num;
-	bool case_insensitive;
-	bool regex;
-	bool print_bytes_offset ;
-	bool print_line_count;
-	bool print_not_matching;
-	bool strict_match_only;
-	int print_multi_lines;
-}Params_struct;
-
-int multi_lines_to_print; // TODO - refactor this global to someplace better
-
-typedef struct line{
-	FILE *input_stream;
-	char *line_content;
-	size_t line_buffer_capacity;
-	int line_num;
-	int line_length;
-	bool is_match_in_line;
-
-}Line;
-
-Params_struct params = {false,false,false,false,false,false,false,0};
 
 int parse_arguments(char *argv[])
 {
@@ -72,8 +50,6 @@ bool is_match_in_line(char *line, char *str_to_find)
 
 int report_line_match(Line *current_line)
 {
-
-
 	if((current_line->is_match_in_line && !params.print_not_matching) ||
 			(!current_line->is_match_in_line && params.print_not_matching)){
 		multi_lines_to_print = params.print_multi_lines;
--- run_test.sh
+++ run_test.sh
@@ -5,7 +5,7 @@ echo "Preforming Valgrind memory check"
 valgrind --leak-check=yes ./my_grep -v -i -E "o\.pdf" grep_tests/2013.html | diff grep_tests/out9 -
 
 echo "Starting tests..."
-echo "----------------------------------------"
+echo "--------------------------------------------------------------------------------"
 ./my_grep a ./grep_tests/emptyfile | diff ./grep_tests/emptyfile -
 ./my_grep bla ./grep_tests/bla | diff ./grep_tests/bla -
 ./my_grep bla ./grep_tests/3lines | diff ./grep_tests/out3 -
@@ -17,5 +17,5 @@ cat ./grep_tests/3lines | ./my_grep bla | diff ./grep_tests/out3 -
 ./my_grep -c -i -E "o\.pdf" grep_tests/2013.html | diff grep_tests/out8 -
 ./my_grep -v -i -E "o\.pdf" grep_tests/2013.html | diff grep_tests/out9 -
 ./my_grep -x -i bla grep_tests/3lines | diff grep_tests/out3 -
-echo "----------------------------------------"
+echo "--------------------------------------------------------------------------------"
 echo "Tests finished!"
