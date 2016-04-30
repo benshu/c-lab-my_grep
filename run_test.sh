6d65ae360fe5973b591a67dc99b6ac9e95f425d9
Hagay Ben-Shushan <hagaybens@gmail.com>
2016-04-30 17:47:50 +0300

    Basic regex implementatoin supporting '.'

--- run_test.sh
+++ run_test.sh
@@ -18,5 +18,6 @@ cat ./grep_tests/3lines | ./my_grep bla | diff ./grep_tests/out3 -
 ./my_grep -v -i -E "o\.pdf" grep_tests/2013.html | diff grep_tests/out9 -
 ./my_grep -x -i bla grep_tests/3lines | diff grep_tests/out3 -
 cat grep_tests/2013.html | ./my_grep -A 2 -n -b -i io | diff grep_tests/myout10 -
+./my_grep -n -E "bl." ./grep_tests/3lines | diff ./grep_tests/myout11 -
 echo "--------------------------------------------------------------------------------"
 echo "Tests finished!"
